c59bef124913a78ecbe0b6a110cd233362fb3bc4
pain1337 <bocharov080@mail.ru>
2020-06-15 03:04:31 +0300

    Fix Test

--- task4/src/com/company/MergeSortTest.java
+++ task4/src/com/company/MergeSortTest.java
@@ -7,24 +7,42 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MergeSortTest {
-    int[] testArray;
+    @Test
+    public void normalTestMergeSort() {
+        int[] testArray = {1, 231, 23, 3, 12, 0, -12, 2, 3, 41, 12, 18, 23, 41, 15};
+        int[] testData = testArray.clone();
 
-    private void fillRndNum(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = new Random().nextInt();
-        }
+        Arrays.sort(testData);
+        testArray = MergeSort.mergeSort(testArray);
+
+        Assert.assertArrayEquals(testData, testArray);
     }
 
     @Test
-    public void testMergeSort() {
-        for(int i = 0; i < 1000; i++) {
-            testArray = new int[new Random().nextInt(100000)];
-            int[] testData = testArray.clone();
+    public void smallTestMergeSort() {
+        int[] testArray1 = {};
+        int[] testData1 = testArray1.clone();
+        int[] testArray2 = {1};
+        int[] testData2 = testArray2.clone();
+        int[] testArray3 = {2, 1};
+        int[] testData3 = testArray3.clone();
+        int[] testArray4 = {3, 1, 2};
+        int[] testData4 = testArray4.clone();
+        Assert.assertArrayEquals(testData1, testArray1);
+        Assert.assertArrayEquals(testData2, testArray2);
+        Assert.assertArrayEquals(testData3, testArray3);
+        Assert.assertArrayEquals(testData4, testArray4);
+    }
 
-            Arrays.sort(testData);
-            testArray = MergeSort.mergeSort(testArray);
+    @Test
+    public void testMergeSortWithSortArr() {
+        int[] testArray = {1, 231, 23, 3, 12, 0, -12, 2, 3, 41, 12, 18, 23, 41, 15};
+        int[] testData = testArray.clone();
 
-            Assert.assertArrayEquals(testData, testArray);
-        }
+        Arrays.sort(testData);
+        Arrays.sort(testArray);
+        testArray = MergeSort.mergeSort(testArray);
+
+        Assert.assertArrayEquals(testData, testArray);
     }
 }
